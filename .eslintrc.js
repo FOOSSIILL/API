3e94bf15f7f7c3ddcacdedab8652aba9148401c5
SofíaMG <98538133+FOOSSIILL@users.noreply.github.com>
2023-03-23 12:29:13 -0500

    .....

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,24 @@
 module.exports = {
-  extends: [
-    'semistandard'
-  ]
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
